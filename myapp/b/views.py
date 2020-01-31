353c160975cd4153b920b47949f2d9b53ec381d0
Kittipot <Kittipot321@gmail.com>
2020-01-31 08:19:06 +0700

    commit

---
+++ myapp/b/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def one(request):
+    return HttpResponse("หน้าจอ dashboard สามารถดูภาพรวมของระบบได้ว่า ในวันนั้น ๆ แต่ละวิชามีจำนวนคนเข้าเรียนกี่คน ขาดกี่คน")
+def two(request):
+    return HttpResponse("หน้าจอค้นหา และ export ข้อมูลการเข้าห้องเรียน ทั้งในเทอมปัจจุบัน และ ย้อนหลัง")
---
+++ myapp/c/urls.py
@@ -0,0 +1,27 @@
+"""myapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('all_student/', views.all_student),
+    path('add_student/', views.add_student),
+    path('edit_student/', views.edit_student),
+    path('all_course/', views.all_course),
+    path('add_course/', views.add_course),
+    path('edit_course/', views.edit_course)
+    
+]
